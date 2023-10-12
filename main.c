e1b27d22c3cd5ddff80a690e77390cd054264468
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-10-12 16:06:31 +0900

    03

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(void)
+int sumTwo (int a, int b)
 {
-     int x;
-     printf("func1 x is at %p\n", &x);
- }
- 
+    int result1;
+    result1 = a + b;
+    return result1;
+    // 그냥 return a+b;도 가능하다. 
+}
+
+int square (int n)
+{
+    int result2;
+    result2 = n*n;
+    return result2;
+    // 그냥 return n*n;도 가능하다. 
+}
+
+int get_max (int x, int y)
+{
+    if ( x > y )
+       return x;
+    else 
+       return y;
+}
+
 
 int main(int argc, char *argv[])
 {
-    int x;
-    printf("main x is at %p\n", &x);
-    func1();
+    int q, w;
+    q = 3;
+    w = 10;
+     
+    printf("sumTwo result : %i\n", sumTwo(q, w));
+    
+    printf("square result : %i\n", square (q));
+    
+    printf("get_max result : %i\n", get_max(q,w));
     
   system("PAUSE");	
   return 0;
