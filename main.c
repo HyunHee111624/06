c30a284f0c96ead4c804f95afe37b64dc3a738d5
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-10-12 16:25:58 +0900

    04

--- main.c
+++ main.c
@@ -1,42 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo (int a, int b)
+int square (int a)
 {
-    int result1;
-    result1 = a + b;
-    return result1;
-    // 그냥 return a+b;도 가능하다. 
-}
-
-int square (int n)
-{
-    int result2;
-    result2 = n*n;
-    return result2;
-    // 그냥 return n*n;도 가능하다. 
-}
-
-int get_max (int x, int y)
-{
-    if ( x > y )
-       return x;
-    else 
-       return y;
-}
-
+     return (a*a);
+ }
 
 int main(int argc, char *argv[])
 {
-    int q, w;
-    q = 3;
-    w = 10;
-     
-    printf("sumTwo result : %i\n", sumTwo(q, w));
-    
-    printf("square result : %i\n", square (q));
-    
-    printf("get_max result : %i\n", get_max(q,w));
+    int a = 2;
+    a = square(a);
+    printf("a=%i\n", a);
     
   system("PAUSE");	
   return 0;
